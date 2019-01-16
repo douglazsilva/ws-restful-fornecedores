61b9a2f462391757339735e58212f81ca9d3e24b
douglazsilva <fdcsone@hotmail.com>
2019-01-16 19:14:11 -0200

    criar fornecedor

--- src/main/java/tk/douglazsilva/wsrestfulfornecedores/fornecedores/FornecedorResource.java
+++ src/main/java/tk/douglazsilva/wsrestfulfornecedores/fornecedores/FornecedorResource.java
@@ -1,13 +1,18 @@
 package tk.douglazsilva.wsrestfulfornecedores.fornecedores;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import tk.douglazsilva.wsrestfulfornecedores.exceptions.FornecedorNotFoundException;
 
@@ -34,5 +39,15 @@ public class FornecedorResource {
 	@DeleteMapping("/fornecedores/{id}")
 	public void deleteFornecedor(@PathVariable long id) {
 		fr.deleteById(id);
+	}
+	
+	@PostMapping("/fornecedores")
+	public ResponseEntity<Object> createFornecedor(@RequestBody Fornecedor fornecedor) {
+		Fornecedor savedFornecedor = fr.save(fornecedor);
+
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(savedFornecedor.getId()).toUri();
+
+		return ResponseEntity.created(location).build();
 	}
 }
