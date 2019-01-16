4abcda07050a160a70a67032f8d0619efc315209
douglazsilva <fdcsone@hotmail.com>
2019-01-16 19:10:30 -0200

    buscar fornecedor específico

--- src/main/java/tk/douglazsilva/wsrestfulfornecedores/fornecedores/FornecedorResource.java
+++ src/main/java/tk/douglazsilva/wsrestfulfornecedores/fornecedores/FornecedorResource.java
@@ -1,10 +1,14 @@
 package tk.douglazsilva.wsrestfulfornecedores.fornecedores;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import tk.douglazsilva.wsrestfulfornecedores.exceptions.FornecedorNotFoundException;
 
 @RestController
 public class FornecedorResource {
@@ -15,5 +19,14 @@ public class FornecedorResource {
 	@GetMapping("/fornecedores")
 	public List<Fornecedor> retrieveAllFornecedores(){
 		return fr.findAll();
+	}
+	
+	@GetMapping("/fornecedores/{id}")
+	public Fornecedor retrieveFornecedor(@PathVariable long id) {
+		Optional<Fornecedor> fornecedor = fr.findById(id);
+		if (!fornecedor.isPresent()) {
+			throw new FornecedorNotFoundException("id -" + id);
+		}
+		return fornecedor.get();
 	}
 }
